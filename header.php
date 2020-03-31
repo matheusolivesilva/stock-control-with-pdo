33546fc9a5791c0b6c5f4a9bab112f7e475e9bc4
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-03-31 10:06:20 -0300

    Create header.php, responsible for navigation
    The header.php was create with the objective to give the power to the
    user to come inside in all the pages of this system.
    
    Trought the header.php the user can:
    
     - Change page
     - Access some categories of this system, suck as "Categories" and
       "Products"

---
+++ header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lant="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device=width, initial-scale='">
+		<title>Stock Control</title>
+		<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+		<link href="assets/css/app.css" rel="stylesheet">
+	</head>
+
+	<body>
+		
+		<nav class="navbar navbar-inverse">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+						<span class="sr-only">Stock Control</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="/index.php">Stock Control</a>
+				</div>
+				<div id="navbar" class="navbar-collapse collapse">
+					<ul class="nav navbar-nav">
+						<li><a href="/categories.php">Categories</a></li>
+						<li><a href="/products.php">Products</a></li>
+					</ul>
+
+				</div>
+			</div>
+		</nav>
+
+		<div class="container">
+
+	</body>
+</html>
