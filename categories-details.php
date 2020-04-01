7dd084ad7dea503ddfe9f4c0885c5d7eb00e17ce
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-04-01 01:04:17 -0300

    Create categories-details.php, create table with data
    It was created to show to the user a data table with informations
    about a product category.
    
    Beside each product in data table, there is a button to edit the
    category.

---
+++ categories-details.php
@@ -0,0 +1,23 @@
+<?php require_once 'header.php' ?>
+<div class="row">
+	<div class="col-md-12">
+		<h2>Category Details</h2>
+	</div>
+</div>
+
+
+<dl>
+	<dt>ID</dt>
+	<dd>1</dd>
+	<dt>Name</dt>
+	<dd>Books</dd>
+	<dt>Products</dt>
+	<dd>
+		<ul>
+			<li><a href="/edit-products.php">The Lord Of Rings</a></li>
+			<li><a href="/edit-products.php">The hitchhiker's guide to the galaxy</a></li>
+		</ul>
+	</dd>
+</dl>
+
+<?php require_once 'footer.php' ?>
