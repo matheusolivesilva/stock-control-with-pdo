308ce481de556e3caeb15429ae58f8fbf65359d4
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-04-01 01:22:39 -0300

    Create edit-categories.php, form to change a category
    This form was created to change a category after click at Edit button
    in product-detail.php or another Edit button.

---
+++ edit-categories.php
@@ -0,0 +1,21 @@
+<?php require_once 'header.php' ?>
+
+<div class="row">
+	<div class="col-md-12">
+		<h2>Edit Category</h2>
+	</div>
+</div>
+<form action="#" method="post">
+	<div class="row">
+		<div class="col-md-6 col-md-offset-3">
+			<div class="form-group">
+				<label for="name"> Category Name</label>
+				<input type="text" value="Books" class="form-control" placeholder="Category Name">
+			</div>
+			<input type="submit" class="btn btn-success btn-block" value="Save">
+		</div>
+	</div>
+</form>
+
+
+<?php require_once 'footer.php' ?>
