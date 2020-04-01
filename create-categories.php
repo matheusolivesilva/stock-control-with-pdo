536170f9b125fa53d814ea43cd35419994071f23
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-04-01 00:28:35 -0300

    Create create-categories.php form
    This form was created with the objective to be the front end form
    that will save the categories in the system.

---
+++ create-categories.php
@@ -0,0 +1,23 @@
+<?php require_once 'header.php';
+
+<div class="row">
+
+	<div class="col-md-12">
+		<h2>New Category</h2>
+	</div>
+</div>
+
+<form action="#" method="post">
+	<div class="row">
+		<div class="col-md-6 col-md-offset-3">
+			<div class="form-group">
+				<label for="name">Category Name</label>
+				<input name="name" type="text" class="form-control" placeholder="Category Name">
+
+			</div>
+			<input type="submit" class="btn btn-success btn-block" value="Save">
+		</div>
+	</div>
+</form>
+
+<?php require_once 'footer.php' ?>
