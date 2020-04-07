d992e654d02dc1d3e25ab7343ef04f7a4fc18ec1
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-04-07 01:55:24 -0300

    Refactor Category.php, use statement and bind value to improve security

--- classes/Category.php
+++ classes/Category.php
@@ -26,34 +26,42 @@ class Category
 
     public function load()
     {
-        $query = "SELECT id, name FROM categories WHERE id = " . $this->id;
+        $query = "SELECT id, name FROM categories WHERE id = :id";
 	$connection = Connection::getConnection();
-	$result = $connection->query($query);
-	$list = $result->fetchAll();
-	foreach ($list as $row) {
-            $this->name = $row['name'];
-	}
+	$stmt = $connection->prepare($query);
+	$stmt->bindValue(':id', $this->id);
+	$stmt->execute();
+	$list = $stmt->fetch();
+        $this->name = $list['name'];
+
     }
   
     public function insert()
     {
-        $query = "INSERT INTO categories (name) VALUES('". $this->name . "')";
+        $query = "INSERT INTO categories (name) VALUES (:name)";
 	$connection = Connection::getConnection();
-	$connection->exec($query);
+	$stmt = $connection->prepare($query);
+	$stmt->bindValue(":name", $this->name);
+	$stmt->execute();
     }
 
 
     public function update()
     {
-        $query = "UPDATE categories SET name = '" . $this->name . "' WHERE id = " . $this->id;
+        $query = "UPDATE categories SET name = :name WHERE id = :id";
         $connection = Connection::getConnection();
-	$connection->exec($query);
+	$stmt = $connection->prepare($query);
+	$stmt->bindValue(':name', $this->name);
+	$stmt->bindValue(':id', $this->id);
+	$stmt->execute();
     }
 
     public function delete()
     {
-        $query = "DELETE FROM categories WHERE id = " . $this->id;
+        $query = "DELETE FROM categories WHERE id = :id";
 	$connection = Connection::getConnection();
-	$connection->exec($query);
+	$stmt = $connection->prepare($query);
+	$stmt->bindValue(':id',$this->id);
+	$stmt->execute();
     }
 }
