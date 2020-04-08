ff4438b4327fd6db3ad904f70ebd9b88bb6e999e
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-04-08 01:05:02 -0300

    Change edit-products.php, load product in the page
    It's being loaded one product data inside each field in this page.

--- edit-products.php
+++ edit-products.php
@@ -1,3 +1,9 @@
+<?php require_once 'global.php' ?>
+<?php
+    $id = $_GET['id'];
+    $product = new Product($id);
+    $listCategories = Category::toList();
+?>
 <?php require_once 'header.php' ?>
 
 <div class"row">
@@ -12,21 +18,29 @@
 		<div class="col-md-6 col-md-offset-3">
 			<div class="form-group">
 				<label form="name">Product Name</label>
-				<input type="text" name="name" value="The Lord of Rings" id="name" class="form-control" placeholder="Product Name" required>
+				<input type="text" name="name" value="<?php echo $product->name ?>" id="name" class="form-control" placeholder="Product Name" required>
 			</div>
 			<div class="form-group">
 				<label form="price">Product Price</label>
-				<input type="number" name="price" value="88.55" step="0.01" min="0" id="price" class="form-control" placeholder="Product Price" required>
+				<input type="number" name="price" value="<?php echo $product->price ?>" step="0.01" min="0" id="price" class="form-control" placeholder="Product Price" required>
 			</div>
 			<div class="form-group">
 				<label for="quantity">Product Quantity</label>
-				<input type="number" name="quantity" value=="8" min="0" id="quantity" class="form-control" placeholder="Product Quantity" require>			
+				<input type="number" name="quantity" value="<?php echo $product->quantity ?>" min="0" id="quantity" class="form-control" placeholder="Product Quantity" require>			
 			</div>
 			<div class="form-group">
 				<label for="category">Product Category</label>
 				<select class="form-control" id="category" name="category_id">
-					<option value="1" selected>Books</option>
-					<option value="2">Magazines</option>
+					<?php $selected = '' ?>
+					<?php foreach($listCategories as $row): ?>
+					<?php
+					    if ($row['id'] == $product->category) {
+					        $selected = 'selected';
+					    }
+					 ?>
+					<option value="<?php echo $row['id'] ?>" <?php echo $selected?>><?php echo $row['name'] ?></option>
+					<?php $selected = '' ?>
+					<?php endforeach ?>
 				</select>
 			</div>
 			<input type="submit" class="btn btn-success btn-block" value="Save">
